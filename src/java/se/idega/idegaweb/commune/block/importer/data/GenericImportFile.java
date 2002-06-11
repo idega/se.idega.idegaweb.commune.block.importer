cbd596e3740a5f05f00370624eb9e8b294bd07d6
Eirikur Hrafnsson <eiki@idega.com>
2002-06-11 19:46:30 +0000

    no message

---
+++ src/java/se/idega/idegaweb/commune/block/importer/data/GenericImportFile.java
@@ -0,0 +1,18 @@
+package se.idega.idegaweb.commune.block.importer.data;
+
+import java.io.File;
+
+/**
+ * <p>Title: IdegaWeb classes</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: Idega Software</p>
+ * @author <a href="mailto:eiki@idega.is"> Eirikur Sveinn Hrafnsson</a>
+ * @version 1.0
+ */
+
+public class GenericImportFile {
+
+  public GenericImportFile() {
+  }
+}
---
+++ src/java/se/idega/idegaweb/commune/block/importer/data/ImportFile.java
@@ -0,0 +1,12 @@
+package se.idega.idegaweb.commune.block.importer.data;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.io.File;
+
+public interface ImportFile{
+
+  public Collection getRecords();
+  public Object getRecordAtIndex(int index);
+  public boolean parse();
+}
