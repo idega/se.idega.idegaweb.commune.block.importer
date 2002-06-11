5c07b491873dc334f81fe881dc3679d266c84db3
Eirikur Hrafnsson <eiki@idega.com>
2002-06-11 12:40:05 +0000

    Initial revision

---
+++ src/java/se/idega/idegaweb/commune/block/importer/business/ImportBusinessBean.java
@@ -0,0 +1,18 @@
+package se.idega.idegaweb.commune.block.importer.business;
+
+import com.idega.business.IBOServiceBean;
+
+/**
+ * <p>Title: IdegaWeb classes</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: Idega Software</p>
+ * @author <a href="mailto:eiki@idega.is"> Eirikur Sveinn Hrafnsson</a>
+ * @version 1.0
+ */
+
+public class ImportBusinessBean extends IBOServiceBean {
+
+  public ImportBusinessBean() {
+  }
+}
