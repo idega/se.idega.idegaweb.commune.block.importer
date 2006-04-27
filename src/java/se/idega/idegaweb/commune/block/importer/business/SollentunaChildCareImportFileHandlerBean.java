51ab00d5946e99b194b6e6413b4c1daf3f39b75e
Dainis Brjuhoveckis <dainis.brjuhoveckis@idega.lv>
2006-04-27 15:57:36 +0000

    Merged in from HEAD: importChildToProvider canCreateMultiple is set to true

--- src/java/se/idega/idegaweb/commune/block/importer/business/SollentunaChildCareImportFileHandlerBean.java
+++ src/java/se/idega/idegaweb/commune/block/importer/business/SollentunaChildCareImportFileHandlerBean.java
@@ -527,8 +527,8 @@ implements ImportFileHandler
 				
 			int schoolID = Integer.parseInt(school.getPrimaryKey().toString());
 			int classID = Integer.parseInt(sClass.getPrimaryKey().toString());
-			boolean importDone = cc.importChildToProvider(-1, ((Integer)child.getPrimaryKey()).intValue(), schoolID, classID, hours, -1, schoolTypeID, null, sDateT, eDateT,
-				locale, parent, performer);
+			boolean importDone = cc.importChildToProvider(-1, ((Integer)child.getPrimaryKey()).intValue(), schoolID, classID, hours, -1, this.schoolTypeID, null, sDateT, eDateT,
+				this.locale, parent, this.performer, true);
 			if (importDone) {
 				try {
 					SchoolClassMember member = cc.getLatestPlacement(((Integer)child.getPrimaryKey()).intValue(), schoolID);
